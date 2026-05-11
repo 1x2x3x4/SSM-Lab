b0681e7788d709b53d94931a54e79b35cca4388f
1x2x3x4 <2368144523@qq.com>
2026-05-11 15:29:19 +0800

    修改了springboot配置文件

--- SpringBoot000_RegisterAdmin/src/main/java/com/news/controller/AdminController.java
+++ SpringBoot000_RegisterAdmin/src/main/java/com/news/controller/AdminController.java
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
+
     @Autowired
     private AdminService adminService;
 
     // 跳转到注册页
     @RequestMapping("/toRegister")
     public String toRegisterAdmin() {
-        // 错误写法：return "/login";
-        // 正确写法：不要加斜杠！自动拼接前缀后缀
-        return "/login";
+        return "login";
     }
 
     // 注册提交
@@ -25,11 +24,11 @@ public class AdminController {
     public String register(String username, String password, Model model) {
         if (adminService.checkAdmin(username) > 0) {
             model.addAttribute("message", "温馨提示: 该用户名已存在！");
-            return "login"; // 这里也不要 redirect，直接返回页面
+            return "login";
         }
         if (adminService.registerAdmin(username, password) > 0) {
             model.addAttribute("message", "温馨提示: 恭喜您，注册成功！");
         }
-        return "/login";
+        return "login";
     }
 }
